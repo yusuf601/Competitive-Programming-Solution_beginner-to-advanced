39038946486116186b67626543a45d7e02f10c20
yusuf601 <yusufmuhyusuh@gmail.com>
2024-08-18 21:08:41 +0700

    resolve for loop problem

--- for loop problem.cpp
+++ for loop problem.cpp
@@ -1,31 +1,21 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <string>
 
 using namespace std;
+
 int main() {
-    
-    
-    string words;
-    words ;
+    int a, b;
+    string words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    cin >> a >> b;
+
+    for (int i = a; i <= b; ++i) {
+        if (i <= 9) {
+            cout << words[i - 1] << endl;
+        } else {
+            cout << (i % 2 == 0 ? "even" : "odd") << endl;
+        }
+    }
+
     return 0;
 }
