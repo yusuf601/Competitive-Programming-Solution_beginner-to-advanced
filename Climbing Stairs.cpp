d2d0ef1126c7d473f3733731e4c4f85957629948
yusuf601 <yusufmuhyusuh@gmail.com>
2024-08-26 14:28:07 +0700

    update:solution

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    int hasil;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        if(n % 2 == 0){
+            hasil = n;
+            
+        }
+    }
+
+
+
+    cin.get();
+    return 0;
+}
+
+
