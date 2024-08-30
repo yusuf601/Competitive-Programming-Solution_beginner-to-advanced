46252acae38208e9d64ae6ed2b3b1c043f78351b
yusuf601 <yusufmuhyusuh@gmail.com>
2024-08-30 09:12:45 +0700

    chore:time convertion

---
+++ src/time convertion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    string jam[] = {"00","01","02","03","04","05","06","07","08","09","10","11","12"};
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
